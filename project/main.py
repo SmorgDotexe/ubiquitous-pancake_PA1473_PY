1e46cba162a9cb890dc7fba9469195d35bc45c8f
Uplink036 <69390820+Uplink036@users.noreply.github.com>
2022-04-08 12:27:21 +0200

    Implented some basic variables and imports.

--- project/main.py
+++ project/main.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env pybricks-micropython
 import sys
 import __init__
+from pybrick.hubs import EV3Hub
+from pybrick.ev3devices import Motor
+from pybrick.parameters import Port
 
-def main(): #Main Class
+EV3 = EV3Hub()
+
+Left_drive = Port.C
+Right_drive = Port.B
+Crane_motor = Port.A
+Front_button = Port.S1
+Light_sensor = Port.S3
+Ultrasonic_sensor = Port.S4
+
+
+def main():  # Main Class
     return 0
+
+
 # Good bye
-#Hej
-if __name__ == '__main__': #Keep this!
+# Hej
+if __name__ == '__main__':  # Keep this!
     sys.exit(main())
